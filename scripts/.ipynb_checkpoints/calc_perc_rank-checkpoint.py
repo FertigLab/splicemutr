91e7dc94775eaa4ada7c18ef79f6a8858898fb70
theron-palmer <theropalm@gmail.com>
2021-08-03 11:28:27 -0400

    calc_perc_rank.py added

---
+++ scripts/.ipynb_checkpoints/calc_perc_rank-checkpoint.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Created: 08/03/2021
+# Created by: Theron Palmer
+
+import numpy as np
+import matplotlib.pyplot as plt
+from bisect import bisect
+
+def main(options, args):
+    data_file = args[0]
+    ref_file = args[1]
+    output_file = args[2]
+    
+    with open(data_file) as dat:
+        data_list = dat.read().splitlines()
+        data_list = data_list[1:]
+    data_vals = [float(i.split(',')[1]) for i in data_list]
+    data_kmers = [i.split(',')[0] for i in data_list]
+    with open(ref_file) as ref:
+        ref_list = ref.read().splitlines()
+        ref_list = ref_list[1:]
+    ref_vals = [float(i.split(',')[1]) for i in ref_list]
+    
+    ref_vals.sort()
+    data_perc = [(bisect(ref_vals,i)/len(ref_vals))*100 for i in data_vals]
+    with open(output_file,'w') as out: 
+        percentile_data = ""
+        for i in range(len(data_kmers)):
+            percentile_data += "%s,%.7f\n"%(data_kmers[i],data_perc[i])
+        print(percentile_data,file=out)
+    
+if __name__ == "__main__":
+
+    from optparse import OptionParser
+
+    parser = OptionParser()
+
+    (options, args) = parser.parse_args()
+
+    main(options, args)
