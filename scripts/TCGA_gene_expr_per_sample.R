da331709843dd2781c8a03305b2295ec23d6ba55
theron-palmer <theropalm@gmail.com>
2021-11-10 18:43:59 -0500

    TCGA_gene_expr_per_sample.R updated

--- scripts/TCGA_gene_expr_per_sample.R
+++ scripts/TCGA_gene_expr_per_sample.R
@@ -14,8 +14,8 @@ library(DESeq2)
 arguments <- parse_args(OptionParser(usage = "",
              description="create gene expression data per sample",
              option_list=list(
-               make_option(c("-o","--output_directory"), 
-                           default = sprintf("%s",getwd()), 
+               make_option(c("-o","--output_directory"),
+                           default = sprintf("%s",getwd()),
                            help="The output directory for the tcga directory"))))
 opt=arguments
 
@@ -25,6 +25,8 @@ tcga_junc_dir <- opt$output_directory
 # reading in the appropriate cancer
 
 cancer <- basename(tcga_junc_dir)
+genotype_file <- sprintf("%s/%s_genotypes.txt",tcga_junc_dir,cancer)
+genotypes <- read.table(genotype_file,header=T)
 
 human_projects <- available_projects()
 proj_info <- subset(
@@ -38,5 +40,6 @@ gene_expression <- rse_dat@assays@data@listData$raw_counts
 # normalizing and saving the gene expression data
 
 gene_expression_norm <- as.data.frame(varianceStabilizingTransformation(gene_expression))
+gene_expression_norm <- gene_expression_norm[,genotypes$external_id]
 
-saveRDS(gene_expression,file=sprintf("%s/gene_expression.rds",tcga_junc_dir))
+saveRDS(gene_expression_norm,file=sprintf("%s/gene_expression_vst.rds",tcga_junc_dir))
