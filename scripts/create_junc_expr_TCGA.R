97d2e27673e6e8e62c889b37b56e3474068858c2
theron-palmer <theropalm@gmail.com>
2021-12-02 01:37:30 -0500

    create_junc_expr_TCGA.R modded

--- scripts/create_junc_expr_TCGA.R
+++ scripts/create_junc_expr_TCGA.R
@@ -22,13 +22,17 @@ junc_dir <- opt$junc_dir
 
 cancer<-basename(junc_dir)
 junc_rse <- readRDS(sprintf("%s/%s_junc_rse.rds",junc_dir,cancer))
+print("extracting juncs")
 junc_expr_comb <- junc_rse@assays@data@listData[["counts"]]
 rm(junc_rse)
 
 coldata <- data.frame(factor(rep("a",ncol(junc_expr_comb))))
 colnames(coldata)<-"samp"
+print("create_deseq")
 junc_expr_comb_dds <- DESeqDataSetFromMatrix(junc_expr_comb,colData = coldata,design=~1)
+print("size_fact")
 junc_expr_comb_dds <- estimateSizeFactors(junc_expr_comb_dds)
+print("dispersions")
 junc_expr_comb_dds <- estimateDispersions(junc_expr_comb_dds,fitType="glmGamPoi")
 
 #------------------------------------------------------------------------------#
@@ -42,7 +46,9 @@ for (i in seq(1,total,100)){
   if (end > total){
     end <- total
   }
+  print("sub")
   junc_expr_comb_sub<-junc_expr_comb_dds[,seq(start,end)]
+  print("vst")
   junc_expr_comb_vst <- vst(junc_expr_comb_sub)
   # junc_expr_comb_vst <- as.data.frame(junc_expr_comb_vst@assays@data@listData[[1]])
   # junc_expr_comb_sub <- as.data.frame(junc_expr_comb_sub@assays@data@listData[[1]])
