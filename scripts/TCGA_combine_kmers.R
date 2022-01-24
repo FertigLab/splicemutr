4d5104d0d2f835821f22c20405759a9f1c5a7d60
theron-palmer <theropalm@gmail.com>
2022-01-24 10:56:33 -0500

    first push since wipe and TCGA_combine_kmers.R updated

--- scripts/TCGA_combine_kmers.R
+++ scripts/TCGA_combine_kmers.R
@@ -35,19 +35,19 @@ genotypes <- read.table(sprintf("%s/%s_genotypes.txt",junc_dir,cancer),header=T)
 for (i in seq(nrow(kmer_files))){
   file <- kmer_files[i,1]
   if (i == 1){
-    kmer_counts_all <- read.table(file,sep="\t")
-    last_bit <- kmer_counts_all[,seq(ncol(kmer_counts_all)-1,ncol(kmer_counts_all))]
-    kmer_counts_all<-kmer_counts_all[,seq(ncol(kmer_counts_all)-2)]
+    kmer_counts_all <- read.table(file,sep="\t",header=T)
+    last_bit <- kmer_counts_all[,1,drop=F]
+    kmer_counts_all<-kmer_counts_all[,seq(2,ncol(kmer_counts_all))]
     kmer_counts_all <- data.frame(apply(kmer_counts_all,2,count_kmers))
   } else {
     kmer_counts_fill <- read.table(file,sep="\t")
-    kmer_counts_fill <- data.frame(apply(kmer_counts_fill[,seq(ncol(kmer_counts_fill)-2)],2,count_kmers))
+    kmer_counts_fill<-kmer_counts_all[,seq(2,ncol(kmer_counts_fill))]
     kmer_counts_all <- cbind(kmer_counts_all,kmer_counts_fill)
   }
 }
 kmer_counts_all <- cbind(last_bit,kmer_counts_all)
 
-colnames(kmer_counts_all)<-c("row","cluster",genotypes$external_id)
+colnames(kmer_counts_all)<-c("row",genotypes$external_id)
 
 #------------------------------------------------------------------------------#
 # saving junction expression file
