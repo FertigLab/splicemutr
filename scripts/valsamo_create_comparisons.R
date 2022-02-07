842c786bb2e91de803dda08daa1ecb47383fecfd
theron-palmer <theropalm@gmail.com>
2022-02-06 22:50:55 -0500

    valsamo_create_comparisons.R updated

--- scripts/valsamo_create_comparisons.R
+++ scripts/valsamo_create_comparisons.R
@@ -27,7 +27,10 @@ arguments <- parse_args(OptionParser(usage = "",
                                help="the junctions directory"),
                    make_option(c("-o","--out_dir"),
                                default = sprintf("%s",getwd()),
-                               help="the output directory"))))
+                               help="the output directory"),
+                   make_option(c("-l","--leaf_dir"),
+                               default = sprintf("%s",getwd()),
+                               help="the leafcutter dir"))))
 opt=arguments
 comparison_juncs_file <- opt$comparison_juncs_file
 splice_dat_file <- opt$splice_dat_file
@@ -35,6 +38,7 @@ comparisons_file <- opt$comparisons_file
 comp_num <- as.numeric(opt$comp_num)
 junc_dir <- opt$junc_dir
 out_dir <- opt$out_dir
+leaf_dir <- opt$leaf_dir
 
 #------------------------------------------------------------------------------#
 # internal functions
@@ -83,9 +87,9 @@ comparison <- names(comparisons)[comp_num]
 if (comparison %in% names(comparison_juncs)){
   targets <- comparisons[[comparison]]$targets
   comparators <- comparisons[[comparison]]$comparators
-  # psi <- data.frame(seq(nrow(splice_dat)))
-  # colnames(psi)<-"rows"
-  # psi$juncs <- splice_dat$juncs
+  psi <- data.frame(seq(nrow(splice_dat)))
+  colnames(psi)<-"rows"
+  psi$juncs <- splice_dat$juncs
 
   kmers <- data.frame(seq(nrow(splice_dat)))
   colnames(kmers)<-"rows"
@@ -103,20 +107,20 @@ if (comparison %in% names(comparison_juncs)){
                                comp_juncs_parsed$V3,
                                strand)
 
-  # psi_all <- data.frame(unique(comp_juncs_parsed))
-  # colnames(psi_all)<-"comp_juncs_parsed"
-  # rownames(psi_all)<-psi_all$comp_juncs_parsed
+  psi_all <- data.frame(unique(comp_juncs_parsed))
+  colnames(psi_all)<-"comp_juncs_parsed"
+  rownames(psi_all)<-psi_all$comp_juncs_parsed
 
   for (target in targets){
-    # psi_file <- sprintf("%s/%s_juncs_%s_perind.counts",leaf_dir,target,comparison)
-    # psi_dat <- read.table(psi_file,header=T)
+    psi_file <- sprintf("%s/%s_juncs_%s_perind.counts",leaf_dir,target,comparison)
+    psi_dat <- read.table(psi_file,header=T)
     juncs <- as.data.frame(matrix(unlist(strsplit(psi_dat$chrom,":")),byrow=T,nrow=nrow(psi_dat)))
     strand <- as.data.frame(matrix(unlist(strsplit(juncs$V4,"_")),byrow=T,nrow=nrow(psi_dat)))
     strand <- strand$V3
     juncs <- sprintf("%s:%s:%s:%s",juncs$V1,juncs$V2,juncs$V3,strand)
-    # psi_dat$juncs <- juncs
-    # psi_dat <- psi_dat %>% dplyr::filter(juncs %in% psi_all$comp_juncs_parsed)
-    # psi_all[psi_dat$chrom,target] <- parse_fraction(psi_dat[,sprintf("%s.filt",str_replace_all(str_replace_all(target,"-","."),"[+]","."))])
+    psi_dat$juncs <- juncs
+    psi_dat <- psi_dat %>% dplyr::filter(juncs %in% psi_all$comp_juncs_parsed)
+    psi_all[psi_dat$chrom,target] <- parse_fraction(psi_dat[,sprintf("%s.filt",str_replace_all(str_replace_all(target,"-","."),"[+]","."))])
     if (file.size(sprintf("%s/%s_splicemutr_kmers.rds",junc_dir,target))!=0){
       target_dat <- readRDS(sprintf("%s/%s_splicemutr_kmers.rds",junc_dir,target))
       kmers[,target]<-target_dat$kmers
@@ -127,13 +131,13 @@ if (comparison %in% names(comparison_juncs)){
   for (comp in comparators){
     if (file.size(sprintf("%s/%s_splicemutr_kmers.rds",junc_dir,comp))!=0){
       comp_dat <- readRDS(sprintf("%s/%s_splicemutr_kmers.rds",junc_dir,comp))
-      # psi_all[psi_dat$chrom,sprintf("%s_comp",comp)] <- parse_fraction(psi_dat[,sprintf("%s.filt",str_replace_all(str_replace_all(comp,"-","."),"[+]","."))])
+      psi_all[psi_dat$chrom,sprintf("%s_comp",comp)] <- parse_fraction(psi_dat[,sprintf("%s.filt",str_replace_all(str_replace_all(comp,"-","."),"[+]","."))])
       kmers[,comp]<-comp_dat$kmers
     } else {
       kmers[,comp]<-""
     }
   }
-  # psi_all[is.na(psi_all)]<-0
+  psi_all[is.na(psi_all)]<-0
   splice_dat$rows <- seq(nrow(splice_dat))
   splice_dat_specific <- splice_dat %>% dplyr::filter(juncs %in% comp_juncs_parsed)
   splice_dat_specific <- splice_dat_specific %>% dplyr::filter(!(verdict == "annotated" & modified == "changed"))
@@ -141,14 +145,14 @@ if (comparison %in% names(comparison_juncs)){
   kmers_specific_parsed <- kmers_specific
   kmers_specific_parsed<-as.data.frame(t(apply(kmers_specific_parsed,1,parse_kmers)))
   colnames(kmers_specific_parsed) <- colnames(kmers_specific)
-  # psi_all <- psi_all[splice_dat_specific$juncs,]
+  psi_all <- psi_all[splice_dat_specific$juncs,]
 
   #------------------------------------------------------------------------------#
   # saving comparison info
 
   saveRDS(splice_dat_specific,file=sprintf("%s/splice_dat_%s.rds",out_dir,comparison))
   saveRDS(kmers_specific_parsed,file=sprintf("%s/kmers_specific_%s.rds",out_dir,comparison))
-  # saveRDS(psi_all,file=sprintf("%s/psi_all_%s.rds",save_dir,comparison))
+  saveRDS(psi_all,file=sprintf("%s/psi_all_%s.rds",save_dir,comparison))
 } else {
   print(sprintf("%s not determined",comparison))
 }
