26066764ce2bc1419403e36d88211be3641e8938
theron-palmer <theropalm@gmail.com>
2022-02-11 22:53:28 -0500

    analyze_splicemutr.R created

---
+++ scripts/analyze_splicemutr.R
@@ -0,0 +1,103 @@
+#!/usr/bin/env Rscript
+
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(optparse)
+
+#------------------------------------------------------------------------------#
+# handling command line input
+
+arguments <- parse_args(OptionParser(usage = "",
+                 description="",
+                 option_list=list(
+                   make_option(c("-g","--genotypes_file"),
+                               default = sprintf("%s",getwd()),
+                               help="the genotypes file"),
+                   make_option(c("-s","--summary_dir"),
+                               default = sprintf("%s",getwd()),
+                               help="summary file dir"),
+                   make_option(c("-d","--splice_dat_file"),
+                               default = sprintf("%s",getwd()),
+                               help="splicemutr file"),
+                   make_option(c("-o","--out_dir"),
+                               default = sprintf("%s",getwd()),
+                               help="output directory"),
+                   make_option(c("-t","--summary_type"),
+                               default = "",
+                               help="summary type"),
+                   make_option(c("-n","--sample_num"),
+                               default = "",
+                               help="the sample num"))))
+opt=arguments
+genotypes_file <- opt$genotypes_file
+summary_dir <- opt$summary_dir
+splice_dat_file <- opt$splice_dat_file
+out_dir <- opt$out_dir
+summary_type <- opt$summary_type
+sample_num <- opt$sample_num
+
+#------------------------------------------------------------------------------#
+# local directories and file inputs for testing
+
+# genotypes_file <- "/media/theron/My_Passport/Valsamo/genotypes/genotypes.rds"
+# summary_dir <- "/media/theron/My_Passport/Valsamo/mhcnuggets_out/run_12072021/predictions_1"
+# splice_dat_file <- "/media/theron/My_Passport/Valsamo/analysis/splicemutr_output/run_12072021/data_splicemutr.txt"
+# counts_file <- "/media/theron/My_Passport/Valsamo/juncs/Q21777-Plate-1-A01_L15.filt.junc"
+
+#------------------------------------------------------------------------------#
+# reading in the data necessary for creating specific splicemutr data
+
+genotypes <- readRDS(genotypes_file)
+splice_dat <- readRDS(splice_dat_file)
+
+#------------------------------------------------------------------------------#
+# reading in the genotype for the specific sample
+
+samples <- names(genotypes)
+sample <-samples[sample_num]
+sample_geno <- unname(genotypes[[which(names(genotypes) == sample)]])
+class_1 <- c("HLA-A","HLA-B","HLA-C")
+sample_geno <- str_replace(sample_geno[which(str_detect(sample_geno,class_1[1]) |
+                                               str_detect(sample_geno,class_1[2]) |
+                                               str_detect(sample_geno,class_1[3]))], ":","-")
+
+sample_kmers <- data.frame(seq(nrow(splice_dat)))
+colnames(sample_kmers) <- "rows"
+rownames(sample_kmers) <- sample_kmers$rows
+sample_kmers$kmers <- NA
+
+sample_kmers_ret <- vapply(seq(length(sample_geno)),function(geno_val){
+  HLA<-sample_geno[geno_val]
+  if (summary_type == "perc"){
+    HLA_summ_file <- sprintf("%s/%s_tx_dict_summary_perc.txt",summary_dir,HLA,summary_type)
+  } else {
+    HLA_summ_file <- sprintf("%s/%s_tx_dict_summary.txt",summary_dir,HLA,summary_type)
+  }
+  if (file.size(HLA_summ_file)!=0){
+    HLA_summ <- read.table(HLA_summ_file,header=F,sep="\t")
+    HLA_summ$V1 <- as.numeric(HLA_summ$V1)+1
+    sample_kmers[HLA_summ$V1,"kmers"] <<- vapply(seq(nrow(HLA_summ)),function(row_val){
+      dat_row_val <- HLA_summ[row_val,"V1"]
+      if (is.na(sample_kmers$kmers[dat_row_val])){
+        HLA_summ[row_val,"V2"]
+      } else {
+        paste(c(sample_kmers$kmers[dat_row_val],HLA_summ[row_val,"V2"]),collapse=":")
+      }
+    },character(1))
+  }
+  return(TRUE)
+},logical(1))
+
+#------------------------------------------------------------------------------#
+# annotating sample_kmers with junctions
+
+juncs <- sprintf("%s:%s:%s:%s",splice_dat$chr,splice_dat$start,splice_dat$end,splice_dat$strand)
+splice_dat$juncs <- juncs
+sample_kmers$juncs <- juncs
+
+#------------------------------------------------------------------------------#
+# saving samples_kmers file
+
+sample_kmers_file <- sprintf("%s/%s_splicemutr_kmers.rds",out_dir,sample)
+saveRDS(sample_kmers,file=sample_kmers_file)
---
+++ scripts/create_comparisons.R
@@ -0,0 +1,73 @@
+#!/usr/bin/env Rscript
+
+library(dplyr)
+library(stringr)
+library(optparse)
+
+#------------------------------------------------------------------------------#
+# handling command line input
+
+arguments <- parse_args(OptionParser(usage = "",
+                 description="",
+                 option_list=list(
+                   make_option(c("-d","--splice_dat_file"),
+                               default = sprintf("%s",getwd()),
+                               help="splicemutr file"),
+                   make_option(c("-s","--sample_files"),
+                               default = sprintf("%s",getwd()),
+                               help="the sample files"),
+                   make_option(c("-o","--out_prefix"),
+                               default = sprintf("%s",getwd()),
+                               help="the output prefix"))))
+opt=arguments
+splice_dat_file <- opt$splice_dat_file
+sample_files<-read.table(optp$sample_files)
+out_prefix <- opt$out_prefix
+
+#------------------------------------------------------------------------------#
+# internal functions
+
+parse_kmers <- function(kmers){
+  a<-unlist(lapply(strsplit(as.character(kmers[seq(3,length(kmers))]),":"),function(kmer_split){
+    kmer_split <- unique(kmer_split)
+    length(kmer_split)-length(which(str_detect(kmer_split,"NAZZZZZZZ")))
+  }))
+  a<-c(as.character(kmers[seq(1,2)]),a)
+  return(a)
+}
+
+#------------------------------------------------------------------------------#
+# reading in the data
+
+splice_dat <- readRDS(splice_dat_file)
+comparisons <- readRDS(comparisons_file)
+
+splice_dat$juncs <- sprintf("%s:%s:%s:%s",splice_dat$chr,splice_dat$start,splice_dat$end,splice_dat$strand)
+
+#------------------------------------------------------------------------------#
+# creating kmer files
+
+kmers <- data.frame(seq(nrow(splice_dat)))
+colnames(kmers)<-"rows"
+kmers$juncs <- splice_dat$juncs
+
+for (file in sample_files$V1){
+  juncs <- as.data.frame(splice_dat$juncs)
+  if (file.size(file)!=0){
+    sample_dat <- readRDS(file)
+    sample <- str_remove(basename(file),"_splicemutr_kmers.rds")
+    kmers[,sample]<-sample_dat$kmers
+  } else {
+    kmers[,sample]<-""
+  }
+}
+
+splice_dat$rows <- seq(nrow(splice_dat))
+kmers_parsed<-as.data.frame(t(apply(kmers,1,parse_kmers)))
+colnames(kmers_parsed) <- colnames(kmers)
+
+#------------------------------------------------------------------------------#
+# saving comparison info
+
+saveRDS(splice_dat,file=sprintf("%s_splice_dat.rds",out_prefix))
+saveRDS(kmers_parsed,file=sprintf("%s_kmers.rds",out_prefix))
