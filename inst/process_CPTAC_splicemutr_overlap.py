d8f77ee4427eb8baaae00c2416baba9f97780fcd
theron-palmer <theropalm@gmail.com>
2022-05-11 23:24:07 -0400

    CPTAC processing files updated

--- inst/process_CPTAC_splicemutr_overlap.py
+++ inst/process_CPTAC_splicemutr_overlap.py
@@ -12,6 +12,7 @@ def main(options, args):
     cptac_data = pd.read_table(options.cptac_file,sep="\t")
     cptac_row = int(options.cptac_row)
     splicemutr_files = pd.read_table(options.splicemutr_files,header=None,names=["file"])
+    
     out_dir = options.out_dir
 
     external_id = cptac_data["TCGA_external_id"].tolist()[cptac_row]
@@ -25,6 +26,10 @@ def main(options, args):
         else:
             cptac_peptides_Kmer+=[pep[i:(i+K)] for i in range(0,len(pep)-K,1)]
     
+    # print(len(cptac_peptides))
+    # print(len(cptac_peptides_Kmer))
+    # print(len(cptac_peptides_below_Kmer))
+
     splicemutr_file=[i for i in splicemutr_files["file"].tolist() if external_id in i]
     splicemutr_file=splicemutr_file[0]
     splicemutr_data=pd.read_table(splicemutr_file,sep="\t")
@@ -32,17 +37,19 @@ def main(options, args):
     kmers=[i for i in kmers if type(i)!=float]
     kmers=":".join(kmers)
     kmers=kmers.split(":")
+    
+    #Kmers_in_exact = [i for i in kmers if i in cptac_peptides_Kmer]
+    Kmers_in_exact = set(kmers).intersection(set(cptac_peptides_Kmer))
+    # print(len(kmers))
+    # print(len(Kmers_in_exact))
+    # print(len(set(kmers)))
+    # print(len(Kmers_in_exact)/len(set(kmers)))
 
-    Kmers_in_exact = [i for i in kmers if i in cptac_peptides_Kmer]
-    Kmers_not_in_exact = [i for i in kmers if i not in Kmers_in_exact]
-    Kmers_in_below = [i for i in Kmers_not_in_exact if i in cptac_peptides_below_Kmer]
     splicemutr_Kmers_in_cptac = Kmers_in_exact
     splicemutr_Kmers_in_cptac += Kmers_not_in_exact
 
-    splicemutr_Kmers_in_cptac_df = pd.DataFrame({"splicemutr_kmers_count":len(kmers),
-    "splicemutr_kmers_in_cptac_count":len(splicemutr_Kmers_in_cptac),
-    "splicemutr_kmers":kmers,
-    "splicemutr_Kmers_in_cptac":splicemutr_Kmers_in_cptac})
+    splicemutr_Kmers_in_cptac_df = pd.DataFrame({"splicemutr_kmers_count":len(set(kmers)),
+    "splicemutr_kmers_in_cptac_count":len(Kmers_in_exact)})
 
     splicemutr_Kmers_in_cptac_df.to_csv(out_dir+"/"+external_id+"_splicemutr_kmers_in_cptac.txt",sep="\t", index=False)
     
