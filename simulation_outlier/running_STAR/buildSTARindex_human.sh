f15ae1b65b78733b8bcf4f0251dc2071f2f114ff
theron-palmer <theropalm@gmail.com>
2024-02-22 10:35:46 -0500

    simulation outlier fixed

---
+++ simulation_outlier/running_STAR/buildSTARindex_human.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --mail-user=tpalme15@jhmi.edu
+#SBATCH --mail-type=end,fail
+#SBATCH --job-name=index_STAR
+#SBATCH --mem=50G
+#SBATCH --output index_star.o
+#SBATCH --error index_star.e
+
+echo $(date)
+
+# load modules
+#module load sharedapps
+module load star
+
+REF_DIR=/GRCh38.gencode.v39
+OUT_DIR=$REF_DIR/STAR_GRCh38.gencode.v39
+
+mkdir -p $OUT_DIR
+
+cd $OUT_DIR
+
+FASTA=$REF_DIR/GRCh38.primary_assembly.genome.fa
+GFF3=$REF_DIR/gencode.v39.primary_assembly.annotation.gtf
+OVERHANG=99
+
+STAR --runMode genomeGenerate --sjdbGTFtagExonParentTranscript Parent --genomeDir $OUT_DIR --genomeFastaFiles $FASTA --sjdbGTFfile $GFF3 --sjdbOverhang $OVERHANG
+
+echo $(date)
---
+++ simulation_outlier/running_STAR/run_STAR.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --mail-user=tpalme15@jhmi.edu
+#SBATCH --mail-type=end,fail
+#SBATCH --job-name=run_STAR
+#SBATCH --mem=50G
+#SBATCH --output star_run.o
+#SBATCH --error star_run.e
+#SBATCH --cpus-per-task 10
+#SBATCH --array=1-8
+
+module load conda
+
+source activate /users/tpalmer/miniconda3/envs/star
+
+echo $(date)
+
+FASTAFILES=/simulated_reads/fasta_files.txt
+FASTAFILE_1=$(sed -n ${SLURM_ARRAY_TASK_ID}p $FASTAFILES)
+FILENAME=$(basename ${FASTAFILE_1//'_1.fasta'/})
+FASTAFILE_2=${FASTAFILE_1//'1.fasta'/'2.fasta'}
+OUT_DIR=/simulated_reads/bams
+mkdir -p $OUT_DIR
+OUT_DIR=$OUT_DIR/$FILENAME
+
+THREADS=10
+
+REFERENCE=STAR_GRCh38.gencode.v39
+
+STAR --readFilesIn $FASTAFILE_1 $FASTAFILE_2 --genomeDir $REFERENCE --runThreadN $THREADS --twopassMode Basic --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.1 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --outFilterType BySJout --outFilterScoreMinOverLread 0.66 --outFilterMatchNminOverLread 0.66 --limitSjdbInsertNsj 1200000 --outFileNamePrefix $OUT_DIR --outSAMstrandField intronMotif --outFilterIntronMotifs None --alignSoftClipAtReferenceEnds Yes --quantMode GeneCounts --outSAMtype BAM Unsorted --outSAMunmapped Within --genomeLoad NoSharedMemory --outSJtype Standard
