d1ed04671db6062a658823658d08564d6e31f566
theron-palmer <theropalm@gmail.com>
2024-03-04 16:11:46 -0500

    analyze_splicemutr* updated

--- python_scripts/analyze_splicemutr_TCGA.py
+++ python_scripts/analyze_splicemutr_TCGA.py
@@ -17,7 +17,7 @@ def main(options, args):
     sample_num = int(options.sample_num)-1
     fasta_file = options.fasta_file
 
-    ref_kmers = generate_ref_kmer_set(fasta_file)
+    ref_kmers = sp.generate_ref_kmer_set(fasta_file)
     
     # processing command line arguments
     rows = range(len(splice_dat.index))
