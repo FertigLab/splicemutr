f80884b568f97318c52f117d5639ae4705f66daf
Theron Palmer <60159296+theron-palmer@users.noreply.github.com>
2024-03-14 14:58:58 -0400

    Update buildSTARindex_human.sh

--- simulation/running_STAR/buildSTARindex_human.sh
+++ simulation/running_STAR/buildSTARindex_human.sh
@@ -16,7 +16,7 @@ OUT_DIR=$REF_DIR/STAR_GRCh38.gencode.v39
 
 mkdir -p $OUT_DIR
 
-cd $OUT_DIR
+cd $REF_DIR
 
 FASTA_URL=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_39/GRCh38.primary_assembly.genome.fa.gz
 GFF3_URL=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_39/gencode.v39.primary_assembly.annotation.gff3.gz
