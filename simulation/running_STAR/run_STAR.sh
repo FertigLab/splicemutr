f6024dbbef891127b1a4d9eaf008028d6e04a340
Theron Palmer <60159296+theron-palmer@users.noreply.github.com>
2024-03-19 11:24:06 -0400

    Update run_STAR.sh

--- simulation/running_STAR/run_STAR.sh
+++ simulation/running_STAR/run_STAR.sh
@@ -23,4 +23,4 @@ THREADS=10
 
 REFERENCE=STAR_GRCh38.gencode.v39
 
-STAR --readFilesIn $FASTAFILE_1 $FASTAFILE_2 --genomeDir $REFERENCE --runThreadN $THREADS --twopassMode Basic --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.1 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --outFilterType BySJout --outFilterScoreMinOverLread 0.66 --outFilterMatchNminOverLread 0.66 --limitSjdbInsertNsj 1200000 --outFileNamePrefix $OUT_DIR --outSAMstrandField intronMotif --outFilterIntronMotifs None --alignSoftClipAtReferenceEnds Yes --quantMode GeneCounts --outSAMtype BAM Unsorted --outSAMunmapped Within --genomeLoad NoSharedMemory --outSJtype Standard
+STAR --genomeDir $GENOME_DIR --readFilesIn ${fastq1} ${fastq2} --twopassMode Basic --outSAMstrandField intronMotif --outFileNamePrefix $outprefix --runThreadN 6 --readFilesCommand zcat --outSAMtype BAM Unsorted --quantMode GeneCounts
